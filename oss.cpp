9afa0af2ce2ea874eb2c2167852488d31cdf6a42
Jackie <jmh37b@mail.umsl.edu>
2026-02-23 19:58:57 -0600

    added system clock and fork() and exec()

--- oss.cpp
+++ oss.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <cstdlib>
-#include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <signal.h>
+#include <cstring>
 
 struct SimulatedClock {
 	unsigned int seconds;
 	unsigned int nanoseconds;
 };
 
-int main(int argc, char **argv) {	
+int main(int argc, char **argv) {
+
 	// sets default of n, s, t, and i to 1 if value not given
         int n = 1;
 	int s = 1;
@@ -20,22 +21,10 @@ int main(int argc, char **argv) {
 	
 	// initializing opt
 	int opt;
-	
 	// accumulator to track current child processes being completed, set at 0
 	int current = 0;
-	
 	// accumulator to track the total number of child processes launched, set at 0
 	int total = 0;
-	
-	int status;
-
-	key_t key = ftok(".", 'x');
-	int shmid = shmget(key, sizeof(SimulatedClock), IPC_CREAT | 0666);
-	
-	SimulatedClock* clock = (SimulatedClock*) shmat(shmid, NULL, 0);
-
-	clock->seconds = 0;
-	clock->nanoseconds = 0;
 
 	// getopt(3) parses options
 	while ((opt = getopt(argc, argv, "hn:s:t:i:")) != -1) {
@@ -44,8 +33,7 @@ int main(int argc, char **argv) {
 			case 'h':
 				std::cout << "To run program:\n\t ./oss -n # -s # -t # -i #\n";
 				std::cout << "Replace # with an integer for n and s, and a float of t and i.\n";
-				exit(0);
-				break;
+				return 0;
 			// number of total child processes
 			case 'n':
 				// converts the string input for n to int and assigns it to n
@@ -88,25 +76,42 @@ int main(int argc, char **argv) {
 	if (i < 0) {
 		std::cout << "Invalid value for i\n";
 		exit(1);
-
+	}
 	if (s > n){
 		exit(1);
 	}
-}
 
-// launch system clock
+	// Shared memory setup
+	key_t key = ftok(".", 'x');
+	int shmid = shmget(key, sizeof(SimulatedClock), IPC_CREAT | 0666);
+	SimulatedClock* clock = (SimulatedClock*) shmat(shmid, NULL, 0);
 
+	clock->seconds = 0;
+	clock->nanoseconds = 0;
 
-	// while loop launches as many child processes as it can, ensuring the number of current running 
-	// child processes are less than the allowed simultaneous number of processes, and that the total 
-	// number of processes complete is less than the number of total child processes needed
+	unsigned int lastLaunchSec = 0;
+	unsigned int lastLaunchNano = 0;
+
+	unsigned int timeLimitSec = (unsigned int)t;
+	unsigned int timeLimitNano = (t - timeLimitSec) * 1000000000;
+
+	int status;
+	
+	bool stopLaunching = false;
+
 	while (total < n || current > 0) {
 		//incrementClock();
 		clock->nanoseconds += 10000000;
 		
-		if (clock->nanoseconds >= 100000000) {
+		if (clock->nanoseconds >= 1000000000) {
 			clock->seconds++;
 			clock->nanoseconds -= 100000000;
+		}
+		// check simulated time limit
+		if (clock->seconds > timeLimitSec ||
+				(clock->seconds == timeLimitSec &&
+				 clock->nanoseconds >= timeLimitNano)){
+			stopLaunching = true;
 		}
 				
 		// check terminated children
@@ -115,12 +120,56 @@ int main(int argc, char **argv) {
 			current--;
 			// clear PCB entry
 		}
+		// check launch interval
+		unsigned int intervalSec = (unsigned int)i;
+		unsigned int intervalNano = (i - intervalSec) * 1000000000;
+
+		unsigned int elapsedSec = clock->seconds - lastLaunchSec;
+		unsigned int elapsedNano;
+
+		if (clock->nanoseconds >= lastLaunchNano) {
+			elapsedNano = clock->nanoseconds - lastLaunchNano;
+		} else {
+			elapsedSec--;
+			elapsedNano = 1000000000 + clock->nanoseconds - lastLaunchNano;
+		}
+		bool intervalPassed =
+			(elapsedSec > intervalSec) ||
+			(elapsedSec == intervalSec && elapsedNano >= intervalNano);
 
 		// possibly launch new child
-		if (current < s && total < n && inter
+		if (!stopLaunching && current < s && total < n && intervalPassed) {
 
-		// maybe print process table
+			pid_t childPid = fork();
+
+			if (childPid == 0) {
+				char secStr[10];
+				char nanoStr[10];
+
+				sprintf(secStr, "%d", 1);
+				sprintf(nanoStr, "%d", 0);
+
+				execl("./worker", "worker", secStr, nanoStr, NULL);
+				exit(1);
+			}
+
+			if (childPid > 0) {
+				current++;
+				total++;
+
+				lastLaunchSec = clock->seconds;
+				lastLaunchNano = clock->nanoseconds;
+			}
+		}
 	}
-	
+	// wait for remaining children
+	while (wait(NULL) > 0);
+
+	std::cout << "OSS terminating\n";
+	std::cout << total << " workers were launched\n";
+
+	shmdt(clock);
+	shmctl(shmid, IPC_RMID, NULL);
+
+	return 0;	
 }
-return 0;
