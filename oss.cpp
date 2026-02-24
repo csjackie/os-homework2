c63f97261d634a022b9626cce6923b9faac569ef
Jackie <jmh37b@mail.umsl.edu>
2026-02-23 18:19:36 -0600

    updates

--- oss.cpp
+++ oss.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <cstdlib>
 #include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <signal.h>
 
-int main(int argc, char **argv) {
-	
-	// sets default of n, s, and t to 1 if value not given
+struct SimulatedClock {
+	unsigned int seconds;
+	unsigned int nanoseconds;
+};
+
+int main(int argc, char **argv) {	
+	// sets default of n, s, t, and i to 1 if value not given
         int n = 1;
 	int s = 1;
-	int t = 1;
+	float t = 1;
+	float i = 1;
+	
 	// initializing opt
 	int opt;
+	
 	// accumulator to track current child processes being completed, set at 0
 	int current = 0;
+	
 	// accumulator to track the total number of child processes launched, set at 0
 	int total = 0;
+	
+	int status;
+
+	key_t key = ftok(".", 'x');
+	int shmid = shmget(key, sizeof(SimulatedClock), IPC_CREAT | 0666);
+	
+	SimulatedClock* clock = (SimulatedClock*) shmat(shmid, NULL, 0);
+
+	clock->seconds = 0;
+	clock->nanoseconds = 0;
 
 	// getopt(3) parses options
-	while ((opt = getopt(argc, argv, "hn:s:t:")) != -1) {
+	while ((opt = getopt(argc, argv, "hn:s:t:i:")) != -1) {
 		switch (opt) {
 			// outputs a help message explaining how to run the program, then exits
 			case 'h':
-				std::cout << "To run program:\n\t ./oss -n # -s # -t #\n";
-				std::cout << "Replace # with an integer.\n";
+				std::cout << "To run program:\n\t ./oss -n # -s # -t # -i #\n";
+				std::cout << "Replace # with an integer for n and s, and a float of t and i.\n";
 				exit(0);
 				break;
 			// number of total child processes
@@ -36,10 +56,15 @@ int main(int argc, char **argv) {
 				// converts the string input for s to int and assigns it to s
 				s = atoi(optarg);
 				break;
-			// number of iterations for each child process
+			// amount of simulated time
 			case 't':
 				// converts the string input for t to int and assigns it to t
-				t = atoi(optarg);
+				t = atof(optarg);
+				break;
+			// minimum interval between launching child processes
+			case 'i':
+				// converts the float input for i to float and assigns it to i
+				i = atof(optarg);
 				break;
 		}
 	}
@@ -60,10 +85,42 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
+	if (i < 0) {
+		std::cout << "Invalid value for i\n";
+		exit(1);
+
 	if (s > n){
 		exit(1);
 	}
-	
-	return 0;
 }
 
+// launch system clock
+
+
+	// while loop launches as many child processes as it can, ensuring the number of current running 
+	// child processes are less than the allowed simultaneous number of processes, and that the total 
+	// number of processes complete is less than the number of total child processes needed
+	while (total < n || current > 0) {
+		//incrementClock();
+		clock->nanoseconds += 10000000;
+		
+		if (clock->nanoseconds >= 100000000) {
+			clock->seconds++;
+			clock->nanoseconds -= 100000000;
+		}
+				
+		// check terminated children
+		pid_t pid = waitpid(-1, &status, WNOHANG);
+		if (pid > 0) {
+			current--;
+			// clear PCB entry
+		}
+
+		// possibly launch new child
+		if (current < s && total < n && inter
+
+		// maybe print process table
+	}
+	
+}
+return 0;
